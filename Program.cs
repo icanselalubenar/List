8db87ca7a837bb5434716ad1708b87d8f4465b9a
Ikhsan Abdul Rafi <107069762+icanselalubenar@users.noreply.github.com>
2023-07-14 13:13:16 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Mahasiswa mhs1 = new Mahasiswa();
+            mhs1.Nim = "1234";
+            mhs1.NamaMahasiswa = "Erik";
+
+            Mahasiswa mhs2 = new Mahasiswa();
+            mhs2.Nim = "1235";
+            mhs2.NamaMahasiswa = "Wely";
+
+            Dosen dosen = new Dosen();
+            dosen.Nik = "0043";
+            dosen.NamaDosen = "Jhono";
+
+            List<Mahasiswa> list = new List<Mahasiswa>();
+            list.Add(mhs1);
+            list.Add(mhs2);
+
+            Console.WriteLine("Nim   Nama");
+            Console.WriteLine("==========");
+            foreach (Mahasiswa mhs in list)
+            {
+                Console.WriteLine("{0}, {1}", mhs.Nim, mhs.NamaMahasiswa);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
